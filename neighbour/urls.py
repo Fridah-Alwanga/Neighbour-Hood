605f4734471bdf4b93b8a5f89f33a860671616b5
Fridah-Alwanga <fridahalwanga6@gmail.com>
2021-01-30 00:17:07 +0300

    update urls delet neighbour

--- neighbour/urls.py
+++ neighbour/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from neibour import views as user_views
+from neighbour import views as user_views
 from django.conf.urls.static import static
 from django.conf import settings
 
