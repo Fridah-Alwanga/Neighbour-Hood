3327834aa1b02b0f20682526428c4eb94850d20c
Fridah-Alwanga <fridahalwanga6@gmail.com>
2021-01-30 00:29:46 +0300

    install bootstrap4

--- hood/settings.py
+++ hood/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'neighbour',
+    'bootstrap4',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
